0609a8d25e30363c1a98038f26cc529855d9c295
Aruna Priyadarshana Bandara <108753678+arunapriyadarshana@users.noreply.github.com>
2024-06-10 21:49:01 +0530

    add education

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,10 +1,12 @@
 import { BackgroundBeams } from "./ui/BackgroundBeam";
-import { GlobeDemo } from "./ui/GlobeDemo";
 import { TypewriterEffect } from "./ui/TypeWritterEffext";
 import Image from "next/image";
 import profileImg from "../public/myImg.png";
 import { Button } from "./ui/MovingBorder";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+import MagicButton from "./ui/MagicButton";
+import { HoverEffect } from "./ui/HoverCard";
+import { eduDetails } from "@/data";
 
 const Hero = () => {
   return (
@@ -28,6 +30,9 @@ const Hero = () => {
               words="I am a full-stack developer."
               className="text-xl md:text-3xl"
             />
+            <div className="sm:flex hidden">
+              <MagicButton title="Download Resume" position="left" icon={""} />
+            </div>
           </div>
           <div className="w-full flex flex-col items-center justify-center">
             <Button>
@@ -39,13 +44,19 @@ const Hero = () => {
                 className="pt-5"
               />
             </Button>
+            <div className="sm:hidden felx mt-10">
+              <MagicButton title="Download Resume" position="left" icon={""} />
+            </div>
           </div>
         </div>
+      </div>
+      <BackgroundBeams />
+      <div className="mx-2 my-7">
         <div>
           <h1 className="heading">
             About <span className="text-purple">me</span>
           </h1>
-          <p className="text-center">
+          <p className="text-center text-base font-normal">
             I am an IT undergraduate at the University of Moratuwa with a GPA of
             3.79. I enjoy solving problems and developing web applications. I
             have experience with Java, Python, JavaScript, React, and Node.js,
@@ -54,8 +65,13 @@ const Hero = () => {
             challenges.
           </p>
         </div>
+        <div className="mt-5 flex flex-col items-center justify-center">
+          <h1 className="text-3xl font-bold">Educations</h1>
+          <div className="flex flex-wrap justify-center items-center">
+            <HoverEffect items={eduDetails} />
+          </div>
+        </div>
       </div>
-      <BackgroundBeams />
     </div>
   );
 };
--- data/index.ts
+++ data/index.ts
@@ -3,3 +3,22 @@ export const navItems = [
   { name: "Projects", link: "#projects" },
   { name: "Contact", link: "#contact" },
 ];
+
+export const eduDetails = [
+  {
+    img: "/uomLogo.png",
+    title: "University of Moratuwa ",
+    time: "Expected in 2026",
+    name: "BScHons in Information Technology",
+    description:
+      "loreum ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, varius velit. ",
+  },
+  {
+    img: "/sclLogo.png",
+    title: "St. Anne's Collage, Kurunegala",
+    time: "2012 - 2020",
+    name: "GCE A/L in Physical Science Stream",
+    description:
+      "lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, varius velit. loreum ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, varius velit.",
+  },
+];
