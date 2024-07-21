2c756f6ede9cd3863ed0ca840ea1ef1dcc726258
Aruna Priyadarshana Bandara <108753678+arunapriyadarshana@users.noreply.github.com>
2024-07-22 00:25:08 +0530

    update cv

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,16 +19,21 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head />
+      {/* Load the Google Analytics script */}
       <Script
         async
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`}
-      ></Script>
-      <Script id="google-analytics">
-        {`window.dataLayer = window.dataLayer || [];
+      />
+      {/* Initialize Google Analytics */}
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-
-          gtag('config', ${process.env.NEXT_PUBLIC_MEASUREMENT_ID});`}
+          gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}', {
+            page_path: window.location.pathname,
+          });
+        `}
       </Script>
       <body className={inter.className}>
         <link rel="icon" href="/logo2.png" />
